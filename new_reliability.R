002bae6b1886c11cb6efee4a10164eb57c75dd09
jbrehmer <jonas.brehmer@h-its.org>
2022-03-29 15:43:04 +0200

    improved new_eliability.R

--- new_reliability.R
+++ new_reliability.R
@@ -22,21 +22,23 @@ subset_index <- rep(TRUE, ndays)
 for (i in 1:length(TI_large)) {
   subset_index[(TI_large[i]-6):TI_large[i]] <- FALSE
 }
+ndays - sum(subset_index)
 
 ## Exclude large earthquakes (second variant)
-Mlimit <- 5.5
+Mlimit <- 5
 TI_large <- events$TI[events$MAG > Mlimit]
 TI_large <- unique(TI_large)
 subset_index <- rep(TRUE, ndays)
 subset_index[c(TI_large, TI_large + 1)] <- FALSE
+ndays - sum(subset_index)
 
 ## Aggregate mean forecasts
 models_agg <- list()
 for (i in 1:nmods) models_agg[[i]] <- rowSums(models[[i]])
 obs_agg <- rowSums(obs)
 # Erase days with large events
-obs_agg <- obs_agg[subset_index]
-for (i in 1:nmods) models_agg[[i]] <- models_agg[[i]][subset_index]
+#bs_agg <- obs_agg[subset_index]
+#for (i in 1:nmods) models_agg[[i]] <- models_agg[[i]][subset_index]
 
 
 ## Reliability diagrams for aggregated number
@@ -44,7 +46,7 @@ for (i in 1:nmods) models_agg[[i]] <- models_agg[[i]][subset_index]
 
 # Reliability diagram
 filePath <- file.path(fpath, "mean_reldiag_ex1.pdf")
-lim1 <- c(0, 4.5)
+lim1 <- c(0, 6)
 pdf(filePath, width = 7.5, height = 8)
 par(mfrow = c(2,2), mar = c(3.6, 3, 2.2, 0.3))
 for (i in 1:nmods) {
@@ -53,7 +55,7 @@ for (i in 1:nmods) {
 dev.off()
 
 # Reliability diagram on log scale
-filePath <- file.path(fpath, "mean_reldiag_ex1_log.pdf")
+filePath <- file.path(fpath, "mean_reldiag_log.pdf")
 lim2 <- c(0.05, lim1[2])
 pdf(filePath, width = 7.5, height = 8)
 par(mfrow = c(2,2), mar = c(3.6, 3, 2.2, 0.3))
@@ -75,7 +77,7 @@ for (i in 1:nmods) {
 dev.off()
 
 # Reliability diagram on log scale
-filePath <- file.path(fpath, "mean_reldiag_ex2_log.pdf")
+filePath <- file.path(fpath, "mean_reldiag_ex2_m5_log.pdf")
 lim2 <- c(0.05, lim1[2])
 pdf(filePath, width = 7.5, height = 8)
 par(mfrow = c(2,2), mar = c(3.6, 3, 2.2, 0.3))
