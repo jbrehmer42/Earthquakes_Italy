6371bf36ea64f33ea862f665a0e529bd5c6008bf
jbrehmer <jonas.brehmer@h-its.org>
2022-04-02 13:14:01 +0200

    improved resampling, introduced uMCB, cMCB

--- new_reliability.R
+++ new_reliability.R
@@ -6,6 +6,99 @@ fpath <- "/home/jbrehmer/Documents/_temp/Case/Plots"
 # Path for R code
 rpath <- "/home/jbrehmer/Documents/Code/Earthquakes_Italy"
 
+## Define modified version of reliability diagram function
+# function to create a reliability diagram
+plotReliability <- function(aggr, y, txt = "", col = "black", lim = NULL, ln = F, resamp = NULL) {
+  # Plots a mean reliability diagram similar
+  # to the one in Gneiting and Resin (2021)
+  # Resampling is done, but this is not as
+  # straightforward, since all forecasts have
+  # to be nonnegative!
+  yf <- isoreg(aggr, y)$yf
+  x <- sort(aggr)
+  res <- y - aggr
+  # calculate score values
+  score <- function(x,y) mean((x-y)^2)
+  s <- score(aggr, y)
+  s_rc <- score(yf, y[order(aggr)])
+  s_mg <- score(mean(y), y)
+  c_rc_ucond = optim(par = 0, fn = function(c) score(aggr + c, y), 
+                     method = "Brent", lower = min(res), upper = max(res))$par
+  s_rc_ucond = score(aggr + c_rc_ucond, y)
+  # calculate decomposition
+  mcb <- s - s_rc
+  umcb = s - s_rc_ucond
+  cmcb = s_rc_ucond - s_rc
+  dsc <- s_mg - s_rc
+  unc <- s_mg
+  # Do resampling
+  if (!missing(resamp)) {
+    res <- y[order(aggr)] - x
+    if (is.numeric(resamp)) n <- round(resamp) + 1 else n <- 5000
+    low <- floor(n * (1 - 0.9)/2)
+    #low2 <- floor(n * (1 - 0.7)/2)
+    up <- n - low
+    #up2 <- n - low2
+    # Resample residuals under assumption of iid residuals
+    resamples <- sapply(1:n, function(z) x + sample(res, length(y)))
+    # use rounding for integer data 
+    resamples <- round(pmax(resamples, 0))
+    yf_resamples <- apply(resamples, 2, function(z) isoreg(x, z)$yf )
+    # sort resamples, include observed values, and correct bias (shift by mean residual)
+    yf_resamples_sorted <- apply(cbind(yf, yf_resamples), 1, sort) - mean(res)
+    # Compute limits for plotting
+    pind <- (x < yf_resamples_sorted[up, ]) & (x > yf_resamples_sorted[low, ])
+    # Compute MCB p-value
+    mcb_resamples <- sapply(1:n, function(i) score(x, resamples[ ,i]) - score(yf_resamples[ ,i], resamples[ ,i]))
+    rank_obs <- rank(c(mcb_resamples, mcb))[n+1]
+    pval <- 1 - (rank_obs - 1)/(n + 1)
+  }
+  # Prepare plots
+  if (missing(lim)) {
+    lim <- c(min(x), max(x))
+    lim <- lim + c(-1,1) * diff(lim) * 0.08
+  }
+  ttl <- paste("Mean reliability", txt)
+  if (ln) {
+    # Modify for log-log plot
+    yf_line <- yf[ (yf > 0) & pind ]
+    x_line <- x[ (yf > 0) & pind ]
+    if (missing(lim))  lim[1] <- max(1e-6, min(yf_line))
+    plot(NULL, xlim = lim, ylim = lim, main = paste(ttl, "(log scale)"), xlab = "",
+         ylab = "", log = "xy")
+  } else {
+    # Standard plot
+    plot(NULL, xlim = lim, ylim = lim, main = ttl, xlab = "", ylab = "")
+    x_line <- x[pind]
+    yf_line <- yf[pind]
+  }
+  if (!missing(resamp)) {
+    # Add consistency band
+    polygon(c(x[pind], rev(x[pind])), c(yf_resamples_sorted[up,pind], rev(yf_resamples_sorted[low,pind])),
+            border = NA, col = "indianred1")
+  }
+  lines(x_line, yf_line, col = col, lwd = 2)
+  abline(a = 0, b = 1, col = "grey40", lty = 2)
+  lim <- par("usr")[1:2]
+  # switch back to "normal" coordinates
+  par(xlog = F)
+  par(ylog = F)
+  title(xlab = "means", ylab = "recalibrated means", line = 2)
+  # add decomposition values
+  offs <- 0.05 * diff(lim)
+  text(x = lim[1] + offs, y = lim[2] - offs, adj = c(0,1),
+       labels = paste0(c("uMCB", "cMCB", "DSC ", "UNC", "SCR"), collapse = "\n"))
+  text(x = lim[1] + 4 * offs, y = lim[2] - offs, adj = c(0,1),
+       labels = paste0(round(c(umcb, cmcb, dsc, unc, s), digits = 3), collapse = "\n"))
+  if (!missing(resamp)) {
+    text(x = lim[1] + 7 * offs, y = lim[2] - offs, adj = c(0,1),
+         paste0("[p = ", round(pval, 3), "]"))
+  }
+  return(invisible(yf))
+}
+
+
+
 # Do Data preparation
 source(file.path(rpath, "data_prep.R"))
 # source functions for scores
@@ -65,8 +158,8 @@ for (i in 1:nmods) {
 dev.off()
 
 # Reliability diagram
-filePath <- file.path(fpath, "mean_reldiag_ex2.pdf")
-lim1 <- c(0, 4.5)
+filePath <- file.path(fpath, "mean_reldiag_new.pdf")
+lim1 <- c(0, 6)
 pdf(filePath, width = 7.5, height = 8)
 par(mfrow = c(2,2), mar = c(3.6, 3, 2.2, 0.3))
 for (i in 1:nmods) {
@@ -75,7 +168,7 @@ for (i in 1:nmods) {
 dev.off()
 
 # Reliability diagram on log scale
-filePath <- file.path(fpath, "mean_reldiag_ex2_m55_log.pdf")
+filePath <- file.path(fpath, "mean_reldiag_new_log.pdf")
 lim2 <- c(0.05, lim1[2])
 pdf(filePath, width = 7.5, height = 8)
 par(mfrow = c(2,2), mar = c(3.6, 3, 2.2, 0.3))
